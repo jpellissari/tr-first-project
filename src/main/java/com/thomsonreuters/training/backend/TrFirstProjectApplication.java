4c0dbdf48a4cda4c65188c4eca0f69bb2887a4ac
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-10-27 10:42:43 -0300

    chore: basic app with gradle and spring

---
+++ src/main/java/com/thomsonreuters/training/backend/TrFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thomsonreuters.training.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomsonreuters/training/backend/TrFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomsonreuters.training.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
