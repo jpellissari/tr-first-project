c3c8f292ffc4ecdaf48da566dc748e76911bfbb7
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-10-28 11:31:24 -0300

    feat: add repository for client

---
+++ src/main/java/com/thomsonreuters/training/backend/clients/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.thomsonreuters.training.backend.clients.repository;
+
+import java.util.UUID;
+import com.thomsonreuters.training.backend.clients.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository  extends JpaRepository<Client, UUID> { }
