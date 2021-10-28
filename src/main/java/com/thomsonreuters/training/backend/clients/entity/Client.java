89212fd52712d76564df4202855b94b58ffe6f61
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-10-28 11:31:05 -0300

    feat: add entity for client

---
+++ src/main/java/com/thomsonreuters/training/backend/clients/entity/Client.java
@@ -0,0 +1,31 @@
+package com.thomsonreuters.training.backend.clients.entity;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"Client\"")
+public class Client {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID identifier;
+  private String name;
+
+  public Client() {}
+
+  public Client(String name) {
+    this.name = name;
+  }
+
+  public UUID getIdentifier() {
+    return identifier;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
