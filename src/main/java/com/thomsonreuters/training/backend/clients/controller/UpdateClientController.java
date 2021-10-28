723b27799c4d198c62009135742d561a7fc79cba
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-10-28 15:58:44 -0300

    feat: add update client endpoint

---
+++ src/main/java/com/thomsonreuters/training/backend/clients/controller/UpdateClientController.java
@@ -0,0 +1,27 @@
+package com.thomsonreuters.training.backend.clients.controller;
+
+import com.thomsonreuters.training.backend.clients.dto.ClientDTO;
+import com.thomsonreuters.training.backend.clients.entity.Client;
+import com.thomsonreuters.training.backend.clients.mapper.ClientMapper;
+import com.thomsonreuters.training.backend.clients.service.UpdateClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UpdateClientController {
+  @Autowired
+  private UpdateClientService updateClientService;
+
+  @PutMapping("/clients/{id}")
+  public ResponseEntity<Object> handle(@PathVariable String id, @RequestBody ClientDTO clientDto) {
+    clientDto.setId(id);
+    Client client = ClientMapper.toEntity(clientDto);
+    this.updateClientService.execute(client);
+
+    return ResponseEntity.ok().build();
+  }
+}
