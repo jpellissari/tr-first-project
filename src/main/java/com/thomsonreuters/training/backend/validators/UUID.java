d026444cc11af3b117e70f254d2bfba51e361895
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-11-09 08:58:50 -0300

    feat: add UUID validation to method and parameter

--- src/main/java/com/thomsonreuters/training/backend/validators/UUID.java
+++ src/main/java/com/thomsonreuters/training/backend/validators/UUID.java
@@ -1,13 +1,15 @@
 package com.thomsonreuters.training.backend.validators;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Target({FIELD})
+@Target({FIELD, METHOD, PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = UUIDValidator.class)
 public @interface UUID {
