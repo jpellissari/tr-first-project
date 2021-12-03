487e8090a8db52895884317e74451ef3640aa0ac
João Henrique Moretti Pellissari <joaohenrique.morettipellissari@thomsonreuters.com>
2021-12-03 11:51:16 -0300

    feat: add cors support

--- src/main/java/com/thomsonreuters/training/backend/controller/JobPositionsController.java
+++ src/main/java/com/thomsonreuters/training/backend/controller/JobPositionsController.java
@@ -15,6 +15,7 @@ import com.thomsonreuters.training.backend.service.JobPositionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/jobs")
 public class JobPositionsController {
